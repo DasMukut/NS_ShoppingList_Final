d2739dd10d299541051571b0ec586a33bbc44bf5
DasMukut <108810054+DasMukut@users.noreply.github.com>
2022-10-25 10:13:19 +0530

    Initial commit

---
+++ src/components/AddItem.js
@@ -0,0 +1,62 @@
+import React, { useContext, useState } from "react";
+import { AppContext } from "../context";
+
+const AddItem = () => {
+  const { dispatchItemEvent } = useContext(AppContext);
+  const [title, setTitle] = useState("");
+  const [quantity, setQuantity] = useState("");
+  const [price, setPrice] = useState("");
+
+  const handleAddItem = () => {
+    if (title !== "" && quantity !== "" && price !== "") {
+      const item = { id: Math.random(), title, quantity, price };
+      dispatchItemEvent("ADD_ITEM", { newItem: item });
+    } else {
+      alert("Please Fill The Input First ");
+    }
+  };
+
+  return (
+    <div>
+      {/* <h3>Add New User</h3> */}
+      <label>Title</label>
+      <input
+        type="text"
+        value={title}
+        onChange={(e) => {
+          setTitle(e.target.value);
+        }}
+        placeholder="Insert Title"
+        required
+      />
+      <br />
+
+      <label>Quantity</label>
+      <input
+        type="text"
+        value={quantity}
+        onChange={(e) => {
+          setQuantity(e.target.value);
+        }}
+        placeholder="0"
+        required
+      />
+      <br />
+
+      <label>Price</label>
+      <textarea
+        type="text"
+        value={price}
+        onChange={(e) => {
+          setPrice(e.target.value);
+        }}
+        required
+        placeholder="0.00"
+      />
+      <br />
+      <button onClick={handleAddItem}>Add Item</button>
+    </div>
+  );
+};
+
+export default AddItem;
---
+++ src/components/Item.js
@@ -0,0 +1,22 @@
+import { useContext } from "react";
+import { AppContext } from "../context";
+// import /item.css
+const Item = ({ item }) => {
+  const { dispatchItemEvent } = useContext(AppContext);
+
+  const handleRemoveItem = () => {
+    dispatchItemEvent("REMOVE_ITEM", { itemId: item.id });
+  };
+  return (
+    <div className="Item">
+      <h3>{item.title}</h3>
+      <h4>{item.quantity}</h4>
+      <h4>{item.price}</h4>
+      <button className="btn" onClick={handleRemoveItem}>
+        Delete
+      </button>
+    </div>
+  );
+};
+
+export default Item;
